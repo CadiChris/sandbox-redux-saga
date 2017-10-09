4754bcddf6dc67ac68482bd9bdf8331229df64d7
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-10-09 12:00:27 +0200

    Vérification qu'une action peut déclencher plusieurs sagas.

--- src/sagas.js
+++ src/sagas.js
@@ -1,5 +1,5 @@
 import { delay } from 'redux-saga'
-import { put, takeEvery, all, call } from 'redux-saga/effects'
+import { put, takeEvery, all, call, select } from 'redux-saga/effects'
 
 export function* helloSaga() {
   console.log('Hello Sagas!')
@@ -7,16 +7,24 @@ export function* helloSaga() {
 
 export function* incrementAsync() {
   yield call(delay, 1000)
-  yield put({type: 'INCREMENT'})
+  yield put({ type: 'INCREMENT' })
 }
 
 export function* watchIncrementAsync() {
   yield takeEvery('INCREMENT_ASYNC', incrementAsync)
 }
+export function * watchAll() {
+  yield takeEvery('*', function* log(action) {
+    const state = yield select();
+    console.log('action', action);
+    console.log('state after', state)
+  })
+}
 
 export default function* rootSaga() {
   yield all([
     helloSaga(),
-    watchIncrementAsync()]
-  )
+    watchIncrementAsync(),
+    watchAll(),
+  ])
 }
